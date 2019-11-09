a1521deed0257b2de45d8ad8c76534c3df34673f
Juwei <153876651@qq.com>
2019-11-10 01:14:45 +0800

    add person

---
+++ GetStart.Test/PersonTest.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GetStart.Test
+{
+    [TestClass]
+    public class PersonTest
+    {
+        [TestMethod]
+        public void PersonTest_Normal()
+        {
+            Person Me = new Person();
+            Me.FirstName = "Juwei";
+            Me.LastName = "Shi";
+
+            Assert.AreEqual("Juwei Shi", Me.ToString());
+        }
+    }
+}
---
+++ GetStart/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GetStart
+{
+    public class Person
+    {
+        public string FirstName {get;set;}
+
+        public string LastName {get;set;}
+
+        public override string ToString(){
+            return this.FirstName + " " + this.LastName;
+        }
+    }
+}
